538cb09f8b9b254038a323fca891202ed651d04a
eugene <paakwesinunoo135@gmail.com>
2023-04-12 20:36:28 +0100

    user model

---
+++ server/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        require:false
+    },
+    email:{
+        type:String,
+        require:false
+    },
+    bio:{
+        type:String,
+        require:false
+    },
+    password:{
+        type:String,
+        require:false
+    },
+    profile:{
+        type:String,
+        require:false
+    },
+    location:{
+        type:String,
+        require:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    isVerified:{
+        type:Boolean,
+        default:false  
+    },
+    isBanned:{
+        type:Boolean,
+        default:false  
+    },
+    subscribers:{
+        type:Array
+    }
+ 
+},{
+    timestamps:true
+})
+module.exports = mongoose.model('User',userSchema)
