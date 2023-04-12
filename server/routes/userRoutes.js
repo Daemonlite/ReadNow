16f2a4b634bdea11c3c5c63887dac65dde3aa458
eugene <paakwesinunoo135@gmail.com>
2023-04-12 21:08:55 +0100

    reg routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const {getUsers,register,loginUser,updateUserInfo,deleteUser,getUserById} = require('../handlers/userHandlers')
+const {verifyToken} = require('../middleware/verify')
+
+router.get('/',verifyToken,getUsers)
+router.post('/register',register)
+router.post('/login',loginUser)
+router.put('/:id',updateUserInfo)
+router.delete('/:id',deleteUser)
+router.get('/:id',getUserById)
+
+  
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ app.use((cors()))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-
+//routes
+app.use('/api/users',require('./routes/userRoutes'))
 
 //server
 app.listen(port,console.log(`SERVER RUNNING ON PORT ${port}`))
