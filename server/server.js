4f0bd6384cdcc059b9a2ed72368b37d1947dec2c
eugene <paakwesinunoo135@gmail.com>
2023-04-12 20:30:37 +0100

    starting out

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const dotenv = require('dotenv' ).config()
+const app = express()
+const cors = require('cors')
+const port = 8000
+
+
+//middlewares
+app.use((cors()))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+
+//server
+app.listen(port,console.log(`SERVER RUNNING ON PORT ${port}`))
