375cf312f0ff200468c1801a27dc22a446c73ccd
eugene <paakwesinunoo135@gmail.com>
2023-04-16 21:05:06 +0100

    add

--- server/handlers/subscriptionHandler.js
+++ server/handlers/subscriptionHandler.js
@@ -23,3 +23,34 @@ const getSubscriberById = async (req, res) => {
       res.status(500).json({ message: "Failed to retrieve Subscriber" });
     }
   };
+
+  const addSubscriber = async (req,res) => {
+    const {userName,userProfile,user} = req.body
+
+    if(!userName || !userProfile || !user){
+        return res.status(400).json({message:"fill in required fields"})
+    }
+
+    let existingUser;
+    try {
+        existingUser = await User.findById(user)
+    } catch (error) {
+        console.log(error)
+    }
+
+    const sub = new subrary({
+        userName,
+        userProfile
+        ,user
+    })
+
+    try {
+        await sub.save()
+        existingUser.Subscriber.unshift(sub)
+        existingUser.save()
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ error: error });
+    }
+    return res.status(201).json(sub);
+  }
