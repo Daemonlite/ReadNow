2debac13d2aa307538cf15c438475977a61f016c
eugene <paakwesinunoo135@gmail.com>
2023-04-17 19:08:57 +0100

    like chapter

---
+++ server/models/likeChapter.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const chapterLikesSchema = new mongoose.Schema({
+    userProfile:{
+        type:String,
+        require:true
+    },
+    userName:{
+        type:String,
+        require:true 
+    },
+    chapter:{
+        type:mongoose.Schema.Types.ObjectId
+      },
+},{
+    timeStamps:true
+})
+
+module.exports = mongoose.model('StoryLikes',chapterLikesSchema)
--- server/routes/libraryRoutes.js
+++ server/routes/libraryRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
-const {getLibraries,getlibraryById,createLibrary,deleteLibrary} = require('../handlers/libraryHandler')
+const {getLibraries,getlibraryById,createLibrary,deleteLibrary,addStory,getUserLibStory,getUserlibStoryById} = require('../handlers/libraryHandler')
 const {verifyToken} = require('../middleware/verify')
 
 router.get('/',verifyToken,getLibraries)
 router.get('/:id',verifyToken,getlibraryById)
 router.post('/',verifyToken,createLibrary)
 router.delete('/:id',verifyToken,deleteLibrary)
+router.get('/user/lib/story',verifyToken,getUserLibStory)
+router.get('/user/lib/story:id',verifyToken,getUserlibStoryById)
+router.post('/user/lib/story',verifyToken,addStory)
 
 module.exports = router
