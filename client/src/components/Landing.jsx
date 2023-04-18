2a113f1e871c4fb46478adaa94c7042fe62d06dd
eugene <paakwesinunoo135@gmail.com>
2023-04-18 20:41:31 +0100

    styling

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='land'>
+        <div className="head">
+            <h1 className="headText">Welcome to ReadNow</h1>
+            <p className="head-para">The world's most-loved social storytelling platform
+    ReadNow connects a global community of millions readers and writers through the power of story.</p>
+
+    <div className="btns">
+    <Button variant='contained'>Start Reading</Button>
+    <Button variant='outlined'>Publish your Story</Button>
+    </div>
+        </div>
+
+        <div className="image">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTADwuZqn6EwTVaa4iplqsDTDONQPDSlVFU2vOTrYH46ql_EAES" alt=""  className='comp'/>
+        </div>
+    </div>
   )
 }
 
