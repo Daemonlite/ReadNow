8972bc2fdc1c77d0fd5d31fcf1e4105d5111144d
eugene <paakwesinunoo135@gmail.com>
2023-04-21 21:39:27 +0100

    SIOL

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import Landing from './components/Landing'
